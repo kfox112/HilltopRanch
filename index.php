14e424f9f637030b6ce7b395cc9c27b9793c3cf3
kfox112 <kyle@Kyles-MacBook-Pro.local>
2019-08-25 16:21:05 -0400

    Sync with changes on site (Frank's note) + add vacation closure notice for 2019.

--- index.php
+++ index.php
@@ -16,11 +16,16 @@
 	<body>
 		<?php include ("includes/header.php");?>
 		<?php include ("includes/gallery.php");?>
+		<div id="alert">Hilltop Ranch will be closed 08/28/2019 thru 09/04/2019.  We apologize for any inconvenience.</div>
 
 		<div id="content">
 			<div id="welcome" class="star">
 				<h2>Welcome</h2>
-				
+				<div style="background: #DDEEFF">
+					<h3>In Loving Memory of Frank Nagel</h3>
+					 <p>Frank Nagel, founder of Hilltop Ranch, passed away peacefully on Saturday September 8, 2018, surrounded by friends and family.  Frank was a splendid mechanic, an equine enthusiast, and a Korean War veteran.  He will be missed by many.  Hilltop Ranch will remain open, managed by Frank's loving wife Kim.  Hours may vary during this adjustment period.</p>
+				</div>
+
 				<h3>About Us</h3>
 				<p>Hilltop Ranch is a beautiful 68 acre farm located in Mohnton, Pennsylvania.  We have been a family owned, operated, and oriented business for over 20 years.  Call us to set up a lesson, ride, or event.  We promise an enjoyable experience that you will never forget.</p>
 				
@@ -38,6 +43,7 @@
 				<h2>Hours</h2>
 
 				<p class="center"><small><em>Hours are by appointment only.  We are available for appointments during the times listed below.</em></small></p>
+				<p class="center" style="color: red;"><small>CLOSED 08/28/19 - 09/04/19</small></p>
 				<hr class="dashed" />
 
 				<dl>
